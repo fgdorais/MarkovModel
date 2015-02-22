a01ac1c0177af4077fcf90da7598022690701da4
François G. Dorais <fgdorais@gmail.com>
2015-02-22 10:16:12 -0500

    MarkovTrainer: Renamed clear_history method to reset. Added sorted option to model method. Cleanup.

--- trainer.py
+++ trainer.py
@@ -3,24 +3,24 @@ from __future__ import unicode_literals # Python 2/3 compatibility #
 from parser import TokenParser
 
 class MarkovTrainer:
-
+    
     def __init__(self, depth=3):
         self.tdata = {'': {}}
         self.depth = depth
         self.thist = []
-
-    def clear_memory(self):
+    
+    def reset(self):
         """Clear the list of remembered tokens"""
         self.thist = []
-        
+    
     def train(self, stream):
         """Train Markov model using text data from stream"""
-
+        
         for tok in TokenParser(stream):
-
+            
             # First, deal with the empty context
             self.tdata[''][tok] = self.tdata[''].get(tok, 0) + 1
-
+            
             # Then, deal with contexts from memory store
             for d in range(len(self.thist)):
                 # Contexts are recorded a space-separated lists
@@ -28,17 +28,20 @@ class MarkovTrainer:
                 if not ctx in self.tdata:
                     self.tdata[ctx] = {}
                 self.tdata[ctx][tok] = self.tdata[ctx].get(tok, 0) + 1
-
+            
             # Update memory store with new token
             self.thist.append(tok)
             if len(self.thist) > self.depth:
                 del self.thist[0]
-
-    def model(self):
+    
+    def model(self, sorted=True):
         """Get learned model"""
-
+        
         model = {}
         for ctx in self.tdata:
             total = sum(self.tdata[ctx].values())
-            model[ctx] = sorted([(tok,float(self.tdata[ctx][tok])/total) for tok in self.tdata[ctx]], key=lambda x: x[1], reverse=True)
+            model[ctx] = [(tok,float(self.tdata[ctx][tok])/total) for tok in self.tdata[ctx]]
+            if sorted:
+                model[ctx].sort(key=lambda x: x[1], reverse=True)
         return model
+
