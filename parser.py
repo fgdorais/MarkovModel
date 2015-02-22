653dda276a78984ad27ca588a88a87a06685cbc7
François G. Dorais <fgdorais@gmail.com>
2015-02-22 10:26:28 -0500

    TokenParser: Unicode literals. Cleanup.

--- parser.py
+++ parser.py
@@ -1,40 +1,42 @@
+from __future__ import unicode_literals # Python 2/3 compatibility #
+
 import codecs
 import unicodedata
 
 class TokenParser:
     """Parse a Unicode text stream into space/control separated tokens
-
+    
     Space and control characters are identified using Unicode general
     category values as defined in section 4.5 of The Unicode Standard,
     Version 7.0.0.
     """
-
+    
     def __init__(self, stream):
         self.stream = codecs.getreader('utf-8')(stream) # Python 2/3 compatibility #
         self.tokens = []
-
+    
     def __iter__(self):
         return self
-
+    
     def __next__(self):
         self.next() # Python 2/3 compatibility #
     
     def next(self):
         """Get next token"""
-
+        
         while not self.tokens:
             try:
                 self.fill()
             except:
                 raise StopIteration            
-
+        
         return self.tokens.pop(0)
-
+    
     def fill(self):
         """Fill the token queue from stream"""
-
+        
         line = self.stream.readline()
-
+        
         if not line:
             raise EOFError
         
@@ -51,3 +53,4 @@ class TokenParser:
         if word:
             # only reached if file does not end with a newline
             self.tokens.append(word)
+
