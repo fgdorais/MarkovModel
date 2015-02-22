e532435b323a2cdd2d05da581a5cedac79bbe9b3
François G. Dorais <fgdorais@gmail.com>
2015-02-22 10:20:23 -0500

    MarkovGenerator: Fixed minor bug in generate. Renamed set_history method to seed. Cleanup.

--- generator.py
+++ generator.py
@@ -4,33 +4,34 @@ import random
 
 class MarkovGenerator:
     """Generate random text using Markov model"""
-
+    
     def __init__(self, model, depth=3):
         self.depth = depth
         self.model = model
         self.thist = []
         self.urand = random.random
-                    
+    
     def __iter__(self):
         return self
-
+    
     def __next__(self):
         self.next()
-
-    def set_history(self, tokens):
-        self.thist = tokens[-self.depth:]
-
+    
     def next(self):
         """Get next random token"""
-
-        # find longest context within model      
+        
+        # find longest context within model
         for i in range(len(self.thist)):
             ctx = ' '.join(self.thist[i:])
             if ctx in self.model:
                 return self.generate(ctx)
+        
         # default to empty context
         return self.generate('')
-
+    
+    def seed(self, tokens):
+        self.thist = tokens[-self.depth:]
+    
     def generate(self, ctx):
         """Generate a random token from given context"""
         
@@ -44,5 +45,7 @@ class MarkovGenerator:
                 return tok
             else:
                 prob -= self.model[ctx][j][1]
-        # default in case of roundoff errors 
-        return self.model[0][0]
+        
+        # default in case of accumulated roundoff errors
+        return self.model[ctx][0][0]
+
